00361746015144b24539c7c72e0cda01ea162ca4
Yevhenii <veret.evg@gmail.com>
2017-07-20 15:18:02 +0300

    Initial Commit

---
+++ YVTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YVTextField
+//
+//  Created by Yevhenii Veretennikov on 7/20/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
